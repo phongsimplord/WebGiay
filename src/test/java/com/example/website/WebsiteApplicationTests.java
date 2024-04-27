e51314562bfee8ed9ca5d51e7b87c60b1def29c8
hungvu <hung.vv@mor.com.vn>
2024-04-27 10:39:24 +0700

    build docker image with dockerfile

--- src/test/java/com/example/website/WebsiteApplicationTests.java
+++ src/test/java/com/example/website/WebsiteApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.website;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WebsiteApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.website;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WebsiteApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
